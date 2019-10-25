d17892a964bcd284beed5f21d4a2aa08eb51b986
Carlos Zambrano <carlosh4@yahoo.com>
2019-10-25 16:23:42 +1100

    adding a VM

--- resources.tf
+++ resources.tf
@@ -53,3 +53,31 @@ resource "aws_security_group" "SG_terraform" {
   }
 
 }
+
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values= ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "ec2_terraform" {
+  ami = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "my_ec2_terraform"
+  }
+
+  subnet_id = "${aws_subnet.subnet2.id}"
+}
